771dbafbb863fa74c4fda075228d7a7ae2123ced
David McIntyre <davidmcintyre@Davids-MacBook-Pro.local>
2016-09-08 19:23:09 -0400

    updating links to reflect path appended to root

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router';
-
+import { Route, hashHistory } from 'react-router';
 import App from './components/App';
 import Appointment from './components/Appointment';
 import Confirmation from './components/Confirmation';
 import NotFoundPage from './components/NotFoundPage.js';
 
 export default (
-  <Route path="/" component={App}>
+  <Route path="/" history={hashHistory} component={App}>
     <Route path="appointment" component={Appointment}/>
     <Route path="confirmation" component={Confirmation}/>
     <Route path="*" component={NotFoundPage}/>
