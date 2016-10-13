08aab6c97750d6f1428291a357520c7fb85d8f23
davidmcintyre <davidpmcintyre@yahoo.com>
2016-10-13 14:59:47 -0400

    notifications working

--- index.js
+++ index.js
@@ -7,42 +7,31 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-// app.get('/', function(request, response) {
-//   // response.render('pages/index');
-//   response.render('dist/index.html');
-// });
-
 app.get('/', function(req, res){
   res.sendfile('/dist/index.html');
-  // res.send('hi')
 });
 
 app.post('/notify', function(req, res){
   var body = req.body;
-  console.log(body);
-  debugger;
+  const deviceToken = body.token;
+
+  var options = {
+        token: {
+            key: './config/APNSAuthKey_N5H7C8MF7S.p8',
+            keyId: "N5H7C8MF7S",
+            teamId: "YHLRFEMTF5",
+        },
+        production: false,
+      };
+
+  var apnProvider = new apn.Provider(options);
+  var note = new apn.Notification();
+  note.badge = 3;
+  note.alert = 'CommuteCall request accepted';
+  note.topic = "com.CommuteCall.reactnative";
+  apnProvider.send(note, deviceToken).then( (result) => {
+  });
 });
-
-// var options = {
-//       token: {
-//           key: '../../APNSAuthKey_N5H7C8MF7S.p8',
-//           keyId: "N5H7C8MF7S",
-//           teamId: "YHLRFEMTF5",
-//       },
-//       production: false,
-//     };
-
-// var apnProvider = new apn.Provider(options);
-// var note = new apn.Notification();
-// note.badge = 3;
-// note.payload = {'messageFrom': 'John Appleseed'};
-// note.topic = "CommuteCall appointment";
-// apnProvider.send(note, deviceToken).then( (result) => {
-//   });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
